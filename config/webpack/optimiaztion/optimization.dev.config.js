95ece374b91a77aa7bef2f794c1ee5bbd8ae419b
Tastaur <trrrrex93@gmail.com>
2021-09-19 13:23:24 +0400

    delete terser-webpack-plugin

--- config/webpack/optimiaztion/optimization.dev.config.js
+++ config/webpack/optimiaztion/optimization.dev.config.js
@@ -1,13 +1,6 @@
-const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = {
     optimization: {
         runtimeChunk: 'single',
         noEmitOnErrors: true,
-        minimizer: [new TerserPlugin({
-            test: /\.(ts|js)x(\?.*)?$/i,
-            parallel: true
-        })],
-        minimize: true
     },
 }
